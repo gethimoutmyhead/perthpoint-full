850b58b4e78bf68b78cb6f262151459e39a36cba
gethimoutmyhead <theshashiman@gmail.com>
2018-03-04 20:56:54 +0800

    dumped a bunch of old unused themes and plugins. downloaded a new one for compression.

--- wp-content/themes/madar-lite/inc/options/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-	/* Silence is golden.
-	   ---
-	   https://retina-theme.com/
-	   madar lite */
-?>
--- wp-content/themes/madar-lite/js/madarlite_customizer.js
+++
@@ -1,5 +0,0 @@
-jQuery(document).ready(function() {
-
-	jQuery('.preview-notice').append('<a class="madarlite-upgrade-to-pro-button" href="https://retina-theme.com/madar-pro-responsive-multipurpose-retina-ready-wordpress-theme/" class="button" target="_blank">{pro}</a>'.replace('{pro}',madarLiteCustomizerObject.pro));
-
-});
